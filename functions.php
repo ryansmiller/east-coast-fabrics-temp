74c6733ffb435001c1b78d2097f9d450ff40e6ac
Ryan Miller <ryansmiller@gmail.com>
2017-12-10 18:54:10 -0500

    Add Bootstrap 4.0.0 Beta 2

--- functions.php
+++ functions.php
@@ -119,13 +119,17 @@ add_action( 'widgets_init', 'east_coast_fabrics_temp_widgets_init' );
 function east_coast_fabrics_temp_scripts() {
 	wp_enqueue_style( 'east-coast-fabrics-temp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'east-coast-fabrics-temp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_style( 'bootstrap-css', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array(), '4.0.0.2');
 
-	wp_enqueue_script( 'east-coast-fabrics-temp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'modernizr-js', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', array(), '2.8.3', false );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', array('jquery'), '1.12.3', true );
+
+	wp_enqueue_script( 'bootstrap-js', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.min.js', array('jquery'), '4.0.0.2', true );
+
+
+
+
 }
 add_action( 'wp_enqueue_scripts', 'east_coast_fabrics_temp_scripts' );
 
--- header.php
+++ header.php
@@ -11,10 +11,11 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
